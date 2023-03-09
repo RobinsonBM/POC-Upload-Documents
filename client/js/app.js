789a63d4809ffac78d2a84b3961241d6de711d22
Robinson Betancur Marin <robinson.betancur@sinapsis.global>
2023-03-09 15:17:25 -0500

    Add alerts in login

--- client/js/app.js
+++ client/js/app.js
@@ -1,56 +1,56 @@
 App = {
-  contracts: {},
-  web3Provider: '',
+    contracts: {},
+    web3Provider: '',
 
-  init: async () => {
-    console.log('Loaded');
-    await App.loadEthereum();
-    await App.loadAccount();
-    await App.loadContracts();
-    await App.renderDocs();
-  },
+    init: async () => {
+        console.log('Loaded');
+        await App.loadEthereum();
+        await App.loadAccount();
+        await App.loadContracts();
+        await App.renderDocs();
+    },
 
-  loadEthereum: async () => {
-    if (window.ethereum) {
-      App.web3Provider = window.ethereum;
-      console.log('Ethereum existe');
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-    } else if (window.web3) {
-      web3 = new Web3(window.web3.currentProvider);
-    } else {
-      console.warn('No ethereum browser is installed. Try it install metamask');
-    }
-  },
+    loadEthereum: async () => {
+        if (window.ethereum) {
+            App.web3Provider = window.ethereum;
+            console.log('Ethereum existe');
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+        } else if (window.web3) {
+            web3 = new Web3(window.web3.currentProvider);
+        } else {
+            console.warn('No ethereum browser is installed. Try it install metamask');
+        }
+    },
 
-  loadAccount: async () => {
-    const accounts = await window.ethereum.request({
-      method: 'eth_requestAccounts',
-    });
-    App.account = accounts[0];
-  },
+    loadAccount: async () => {
+        const accounts = await window.ethereum.request({
+            method: 'eth_requestAccounts',
+        });
+        App.account = accounts[0];
+    },
 
-  loadContracts: async () => {
-    // Se trae el JSON del contrato desplegado
-    const res = await fetch('FileContract.json');
-    const fileContractJSON = await res.json();
+    loadContracts: async () => {
+        // Se trae el JSON del contrato desplegado
+        const res = await fetch('FileContract.json');
+        const fileContractJSON = await res.json();
 
-    // Se convierte el JSON
-    App.contracts.fileContract = await TruffleContract(fileContractJSON);
+        // Se convierte el JSON
+        App.contracts.fileContract = await TruffleContract(fileContractJSON);
 
-    // Se conecta el contrato a MetaMask
-    App.contracts.fileContract.setProvider(App.web3Provider);
+        // Se conecta el contrato a MetaMask
+        App.contracts.fileContract.setProvider(App.web3Provider);
 
-    // Se conecta y utiliza el contrato desplegado
-    App.fileContract = await App.contracts.fileContract.deployed();
-  },
+        // Se conecta y utiliza el contrato desplegado
+        App.fileContract = await App.contracts.fileContract.deployed();
+    },
 
-  renderDocs: async () => {
-    // const counter = await App.fileContract.docCounter();
-    // const docCounter = counter.toNumber();
-    let data = await App.getDocumentBD();
-    let html = '';
-    data.forEach((element) => {
-      let docElement = `
+    renderDocs: async () => {
+        // const counter = await App.fileContract.docCounter();
+        // const docCounter = counter.toNumber();
+        let data = await App.getDocumentBD();
+        let html = '';
+        data.forEach((element) => {
+            let docElement = `
        <tr>
          <th scope="row">${element.id}</th>
          <td>${element.Titulo}</td>
@@ -61,154 +61,124 @@ App = {
          <td>${element.Grupo}</td>
        </tr>
        `;
-      html += docElement;
-    });
-    // for (let i = 0; i < data.length; i++) {
-    //   // const doc = await App.fileContract.forDoc(i);
-    //   const doc = data[i];
-    //   const docId = doc.id;
-    //   const docTitle = doc.Titulo;
-    //   const docDesc = doc.Descripcion;
-    //   const docOwner = doc.DocOwner;
-    //   // const docDate = doc['createdAt'];
-    //   const docGroup = doc.Grupo;
+            html += docElement;
+        });
+        document.querySelector('#documents').innerHTML = html;
+    },
 
-    //   let docElement = `
-    //   <tr>
-    //     <th scope="row">${docId}</th>
-    //     <td>${docTitle}</td>
-    //     <td>${docDesc}</td>
-    //     <td>Certificados</td>
-    //     <td>${new Date(docDate * 1000).toLocaleString()}</td>
-    //     <td>${docOwner}</td>
-    //     <td>${docGroup}</td>
-    //   </tr>
-    //   `;
-
-    //   html += docElement;
-    // }
-    document.querySelector('#documents').innerHTML = html;
-  },
-
-  getDocument: async (hash) => {
-    try {
-      const docExist = await App.fileContract.getDocument(hash, {
-        from: App.account,
-        gasLimit: 3000000,
-      });
-      const docTx = docExist.receipt;
-      const docLog = docExist.logs[0].args;
-      if (docExist) {
-        iziToast.success({
-          class: 'alert-success',
-          close: true,
-          layout: 2,
-          maxWidth: 500,
-          message: `
+    getDocument: async (hash) => {
+        try {
+            const docExist = await App.fileContract.getDocument(hash, {
+                from: App.account,
+                gasLimit: 3000000,
+            });
+            const docTx = docExist.receipt;
+            const docLog = docExist.logs[0].args;
+            if (docExist) {
+                iziToast.success({
+                    class: 'alert-success',
+                    close: true,
+                    layout: 2,
+                    maxWidth: 500,
+                    message: `
           <strong>Titulo:</strong> ${docLog['DocName']}
           <br><strong>Decripcion:</strong> ${docLog['DocDesc']}
           <br><strong>Fecha de Carga:</strong> ${new Date(
-            docLog['createdAt'] * 1000
+              docLog['createdAt'] * 1000
           ).toLocaleString()}
           <br><strong>Dueño:</strong> ${docLog['DocOwner']}
           <br><strong>Grupo:</strong> ${docLog['group']}
           `,
-          messageColor: '#000000',
-          messageSize: '14px',
-          position: 'topCenter',
-          title: 'EL CERTIFICADO ES CORRECTO SEGÚN BLOCKCHAIN',
-          titleColor: '#25C554',
-          titleSize: '16px',
-          timeout: null,
-          progressBar: false,
-        });
-      }
-    } catch (error) {
-      iziToast.error({
-        backgroundColor: '#F0E5E5',
-        class: 'alert-error',
-        close: true,
-        layout: 2,
-        maxWidth: 500,
-        message: 'Tu contrato no es valido',
-        messageColor: '#000000',
-        messageSize: '14px',
-        position: 'topCenter',
-        title: 'EL CERTIFICADO ES INVALIDO',
-        titleColor: '#E21414',
-        titleSize: '16px',
-        timeout: null,
-        progressBar: false,
-      });
-    }
-  },
-
-  addDocument: async (hash, title, desc, group) => {
-    try {
-      const DocUpload = await App.fileContract.addDocument(
-        hash,
-        title,
-        desc,
-        group,
-        {
-          from: App.account,
+                    messageColor: '#000000',
+                    messageSize: '14px',
+                    position: 'topCenter',
+                    title: 'EL CERTIFICADO ES CORRECTO SEGÚN BLOCKCHAIN',
+                    titleColor: '#25C554',
+                    titleSize: '16px',
+                    timeout: null,
+                    progressBar: false,
+                });
+            }
+        } catch (error) {
+            iziToast.error({
+                backgroundColor: '#F0E5E5',
+                class: 'alert-error',
+                close: true,
+                layout: 2,
+                maxWidth: 500,
+                message: 'Tu contrato no es valido',
+                messageColor: '#000000',
+                messageSize: '14px',
+                position: 'topCenter',
+                title: 'EL CERTIFICADO ES INVALIDO',
+                titleColor: '#E21414',
+                titleSize: '16px',
+                timeout: null,
+                progressBar: false,
+            });
         }
-      );
-      const docLog = DocUpload.logs[0].args;
-      if (DocUpload) {
-        iziToast.success({
-          class: 'alert-success',
-          close: true,
-          layout: 2,
-          maxWidth: 500,
-          message: `
+    },
+
+    addDocument: async (hash, title, desc, group) => {
+        try {
+            const DocUpload = await App.fileContract.addDocument(hash, title, desc, group, {
+                from: App.account,
+            });
+            const docLog = DocUpload.logs[0].args;
+            if (DocUpload) {
+                iziToast.success({
+                    class: 'alert-success',
+                    close: true,
+                    layout: 2,
+                    maxWidth: 500,
+                    message: `
           <strong>Titulo:</strong> ${docLog['DocName']}
           <br><strong>Decripcion:</strong> ${docLog['DocDesc']}
           <br><strong>Fecha de Carga:</strong> ${new Date(
-            docLog['createdAt'] * 1000
+              docLog['createdAt'] * 1000
           ).toLocaleString()}
           <br><strong>Dueño:</strong> ${docLog['DocOwner']}
           <br><strong>Grupo:</strong> ${docLog['group']}
           `,
-          messageColor: '#000000',
-          messageSize: '14px',
-          onClosing: function () {
-            window.location.reload();
-          },
-          progressBar: false,
-          position: 'topCenter',
-          title: 'TU ARCHIVO SE HA CARGADO CORRECTAMENTE EN LA BLOCKCHAIN',
-          titleColor: '#25C554',
-          titleSize: '16px',
-          timeout: null,
-        });
-      }
-      return {
-        update: true,
-        DocOwner: docLog['DocOwner'],
-        createdAt: docLog['createdAt'],
-      };
-    } catch (error) {
-      iziToast.error({
-        backgroundColor: '#F0E5E5',
-        class: 'alert-error',
-        close: true,
-        layout: 2,
-        maxWidth: 500,
-        position: 'topCenter',
-        progressBar: false,
-        timeout: null,
-        title: 'TU ARCHIVO NO SE PUDO CARGAR A LA BLOCKCHAIN',
-        titleColor: '#E21414',
-        titleSize: '16px',
-      });
-      return false;
-    }
-  },
+                    messageColor: '#000000',
+                    messageSize: '14px',
+                    onClosing: function () {
+                        window.location.reload();
+                    },
+                    progressBar: false,
+                    position: 'topCenter',
+                    title: 'TU ARCHIVO SE HA CARGADO CORRECTAMENTE EN LA BLOCKCHAIN',
+                    titleColor: '#25C554',
+                    titleSize: '16px',
+                    timeout: null,
+                });
+            }
+            return {
+                update: true,
+                DocOwner: docLog['DocOwner'],
+                createdAt: docLog['createdAt'],
+            };
+        } catch (error) {
+            iziToast.error({
+                backgroundColor: '#F0E5E5',
+                class: 'alert-error',
+                close: true,
+                layout: 2,
+                maxWidth: 500,
+                position: 'topCenter',
+                progressBar: false,
+                timeout: null,
+                title: 'TU ARCHIVO NO SE PUDO CARGAR A LA BLOCKCHAIN',
+                titleColor: '#E21414',
+                titleSize: '16px',
+            });
+            return false;
+        }
+    },
 
-  getDocumentBD: async () => {
-    let resp = await fetch('http://localhost:5000/get/Blockchain');
-    let { data } = await resp.json();
-    return data;
-  },
+    getDocumentBD: async () => {
+        let resp = await fetch('http://localhost:5000/get/Blockchain');
+        let { data } = await resp.json();
+        return data;
+    },
 };
--- client/js/login.js
+++ client/js/login.js
@@ -1,7 +1,7 @@
 const api = 'http://localhost:3000';
 
 async function loginAuth(body) {
-  return await axios.post(`${api}/post/loginAuth`, body);
+    return await axios.post(`${api}/post/loginAuth`, body);
 }
 
 const form = document.querySelector('#login');
@@ -9,33 +9,48 @@ const url = window.location.origin;
 form.addEventListener('submit', login);
 
 function login(event) {
-  event.preventDefault();
-  const user = {
-    user: event.target['email'].value,
-    pass: event.target['pass'].value,
-  };
-  loginAuth(user)
-    .then((resp) => {
-      const user = resp.data.session.name;
-      const session = JSON.stringify(resp.data.session);
-      sessionStorage.setItem('userLog', user);
-      sessionStorage.setItem('session', session);
-      iziToast.success({
-        class: 'alert-success',
-        layout: 2,
-        maxWidth: 500,
-        message: 'Inicio de Seccion correcto',
-        messageColor: '#000000',
-        messageSize: '14px',
-        position: 'topCenter',
-        progressBar: true,
-        timeout: false,
-        onClosing: () => {
-          window.location.href = url + '/admin.html';
-        },
-      });
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    event.preventDefault();
+    const user = {
+        user: event.target['email'].value,
+        pass: event.target['pass'].value,
+    };
+    loginAuth(user)
+        .then((resp) => {
+            const user = resp.data.name;
+            const session = JSON.stringify(resp.data);
+            document.cookie = session;
+            sessionStorage.setItem('userLog', user);
+            sessionStorage.setItem('session', session);
+            iziToast.success({
+                class: 'alert-success',
+                close: true,
+                layout: 2,
+                maxWidth: 500,
+                position: 'topCenter',
+                progressBar: true,
+                timeout: 3000,
+                title: 'Inicio de Seccion correcto',
+                titleColor: '#25C554',
+                titleSize: '14px',
+                onClosing: () => {
+                    window.location.href = url + '/admin.html';
+                },
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+            iziToast.error({
+                backgroundColor: '#F0E5E5',
+                class: 'alert-error',
+                close: true,
+                layout: 2,
+                maxWidth: 500,
+                position: 'topCenter',
+                title: 'Error al iniciar Sesion',
+                titleColor: '#E21414',
+                titleSize: '14px',
+                timeout: 3000,
+                progressBar: true,
+            });
+        });
 }
